498c91268347c7be63e63feaab39467e4961b327
Anika Anjum <anika15-11488@diu.edu.bd>
2023-07-19 02:20:13 +0600

    Perform BETWEEN  & NOT BETWEEN operator

---
+++ Between.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Fees
+WHERE due BETWEEN 1000 and 3000;
+
+
+SELECT * FROM Fees
+WHERE credit Not BETWEEN 20 and 25;
+
+
+SELECT * FROM Student
+WHERE dob BETWEEN '12/01/2000' and '12/01/2010';
+
+
+SELECT * FROM Student
+WHERE department Not BETWEEN 'Bangla' and 'Math';
