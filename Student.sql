8bf17f2bc015d04010843cc23858732a3e494f9a
Anika Anjum <anika15-11488@diu.edu.bd>
2023-07-19 00:41:03 +0600

    Created Student table

---
+++ Student.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Student(
+	SID int,
+    Name varchar(255),
+    Department varchar(255),
+    Gender varchar(255),
+    DOB DATE,
+    Address varchar(255),
+    Country varchar(255),
+    PostalCode int,
+    PRIMARY KEY (SID)
+    
+);
+
+
+  
+  
+  
+  
