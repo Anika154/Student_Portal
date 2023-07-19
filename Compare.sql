f909f20ab224974b3f24d9e545c4a37b126055ea
Anika Anjum <anika15-11488@diu.edu.bd>
2023-07-19 21:40:49 +0600

    Perform different operators

---
+++ Compare.sql
@@ -0,0 +1,26 @@
+SELECT * FROM Fees
+WHERE due = 2000; 
+
+
+SELECT * FROM Fees
+WHERE due > 2000;
+
+
+SELECT * FROM Fees
+WHERE due >= 2000;
+
+
+SELECT * FROM Fees
+WHERE due < 3000;
+
+
+SELECT * FROM Fees
+WHERE due <= 3000;
+
+
+SELECT * FROM Fees
+WHERE due <> 3000;
+
+
+SELECT * FROM Fees
+WHERE due != 3000;
